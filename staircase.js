2469709e4e92fe453dcff9577692afbe2e48fd5a
A very unfunny developer <74575128+GitPoopHub@users.noreply.github.com>
2024-10-13 17:13:37 -0400

    Update staircase.js

--- staircase.js
+++ staircase.js
@@ -1,45 +1,44 @@
 addIngredient("staircase", {
-    "color": [
-        "8b6950"
-    ],
-    "shape": "cuboid_stairs",
-    "keywords": "stairs",
-    "hidden": true,
-    "broken": "sawdust"
+    color:"8b6950",
+    shape:"cuboid_stairs",
+    keywords:"stairs",
+    broken:"sawdust"
 });
 
 addIngredient("sawdust", {
-    "type": "powder",
-    "color": [
-        "8b6950"
-    ],
-    "shape": "powder_rough",
-    "keywords": "wood",
-    "hidden": true,
-    reactions: {
-        water: { set1:"paper" , set2:null }
+    type:"powder",
+    color:"8b6950",
+    shape:"powder_rough",
+    keywords:"wood",
+    reactions:{
+        water:{set1:"paper"}
     }
 });
 
 addIngredient("paper",{
-    "color": "#e3e9f2",
-    "stackShape": "rectangle_thinnest",
-    "hidden": true,
-    "adj": "writable",
-    "height": "0.2",
-    "shape": "rectangle_fold_top_right",
+    color: "#e3e9f2",
+    stackShape:"rectangle_thinnest",
+    adj:"writable",
+    height:"0.2",
+    shape: "rectangle_fold_top_right",
     reactions: {
-        microplastic: { set1:"confetti" , set2:null }
+        microplastic:{set1:"confetti"}
     }
 });
 
 addIngredient("confetti",{
-    "type": "powder",
-    "color": ["#d93e48","#ef7d57","#ffe478","#a7f070","#00a3ca","#c53a9d"],
-    hidden:true
+    type:"powder",
+    color:"#d93e48",
+    innerColor:"#ef7d57",
+    glow:"#ffe478",
+    cookColor:"#a7f070",
+    shape:"powder_wavy"
 });
+addRecipe("&stack:staircase+doll+soy_sauce","incident");
 
-addRecipe("&stack:staircase+doll+soy_sauce", "incident");
-addRecipe("paper+sand", "sandpaper");
-addRecipe("&stack:paper+sawdust+cream", "sawdust tortilla");
-addRecipe("crab+confetti", "crab rave")
+addRecipe("paper+sand","sandpaper");
+
+addRecipe("&stack:paper+sawdust+cream","sawdust tortilla");
+
+addRecipe("crab+confetti","crab rave")
+})
